4e91de711d335c9746307a251e3bfc27dbccdcc6
sartorio12 <msart2112@gmail.com>
2023-11-20 09:59:05 -0300

    ll

--- whatspy.py
+++
@@ -1,8 +0,0 @@
-from AllWhatsPy import AllWhatsPy
-
-awp = AllWhatsPy()
-
-awp.conexao(server_host=True)
-awp.ctt.encontrar_usuario(11954309528)
-awp.msg.enviar_mensagem('Hello World')
-awp.msg.analise.ultima_mensagem_chat()
